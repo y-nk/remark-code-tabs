0d26708a83a9bc634adffc4169892f98a1cc7d2e
Julien Barbay <julien.barbay@gmail.com>
2023-01-16 14:03:23 +0700

    feat: use package.json version rather than latest when using unpkg

--- style.js
+++ style.js
@@ -1,9 +1,9 @@
-import { name } from './package.json' assert { type: 'json' }
+import { name, version } from './package.json' assert { type: 'json' }
 
 import style from './style.css' assert { type: 'text' }
 
 export default function createStyle(preferCdn = false) {
   return preferCdn
-    ? `<link rel="stylesheet" href="//unpkg.com/${name}@latest/style.css">`
+    ? `<link rel="stylesheet" href="//unpkg.com/${name}@${version}/style.css">`
     : `<style rel="remark-code-tabs">${style}</style>`
 }
