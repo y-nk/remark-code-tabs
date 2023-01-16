e9be36dd3162a5229729a2f85f748075caf1d5b1
Julien Barbay <julien.barbay@gmail.com>
2023-01-16 14:31:13 +0700

    fix: use url to load assets

--- style.js
+++ style.js
@@ -2,11 +2,17 @@ import { readFileSync } from 'fs'
 
 // import { name, version } from './package.json' assert { type: 'json' }
 const { name, version } = JSON.parse(
-  readFileSync('./package.json', 'utf-8')
+  readFileSync(
+    new URL('package.json', import.meta.url),
+    'utf-8',
+  )
 )
 
 // import style from './style.css' assert { type: 'text' }
-const style = readFileSync('./style.css', 'utf-8')
+const style = readFileSync(
+  new URL('style.css', import.meta.url),
+  'utf-8',
+)
 
 export default function createStyle(preferCdn = false) {
   return preferCdn
