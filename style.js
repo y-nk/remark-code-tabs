fe6389d237724b65fcca291895344e38df09a2ef
Julien Barbay <julien.barbay@gmail.com>
2023-01-16 14:11:55 +0700

    fix: avoid import assert

--- style.js
+++ style.js
@@ -1,6 +1,12 @@
-import { name, version } from './package.json' assert { type: 'json' }
+import { readFileSync } from 'fs'
 
-import style from './style.css' assert { type: 'text' }
+// import { name, version } from './package.json' assert { type: 'json' }
+const { name, version } = JSON.parse(
+  readFileSync('./package.json', 'utf-8')
+)
+
+// import style from './style.css' assert { type: 'text' }
+const style = readFileSync('./style.css', 'utf-8')
 
 export default function createStyle(preferCdn = false) {
   return preferCdn
